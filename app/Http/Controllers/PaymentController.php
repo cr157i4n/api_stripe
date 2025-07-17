ef09e23f74b45493f2eba31239dd3244178e9890
Cristian <cccristian7090@gmail.com>
2025-07-17 14:21:40 -0400

    template

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Stripe\Stripe;
+use Stripe\Checkout\Session;
+
+class PaymentController extends Controller
+{
+    public function __construct()
+    {
+        Stripe::setApiKey(config('services.stripe.secret_key'));
+    }
+
+    public function createPaymentLink(Request $request)
+    {
+        try {
+            $session = Session::create([
+                'payment_method_types' => ['card'],
+                'line_items' => [
+                    [
+                        'price_data' => [
+                            'currency' => 'usd',
+                            'product_data' => [
+                                'name' => 'Producto de ejemplo',
+                                'description' => 'Descripción del producto',
+                            ],
+                            'unit_amount' => 2000,
+                        ],
+                        'quantity' => 1,
+                    ],
+                ],
+                'mode' => 'payment',
+                'success_url' => url('/payment/success'),
+                'cancel_url' => url('/payment/cancel'),
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'checkout_url' => $session->url,
+                'session_id' => $session->id
+            ]);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error al crear el link de pago: ' . $e->getMessage()
+            ], 500);
+        }
+    }
+
+
+}
